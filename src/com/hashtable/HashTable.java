99e4a04e34a8564e17124634d501f037686387a4
iamdipaliwankhade <iamdipaliwankhade@gmail.com>
2022-10-21 10:34:16 +0530

    [Dipali] Added : frequency of word in paragraph

--- src/com/hashtable/HashTable.java
+++ src/com/hashtable/HashTable.java
@@ -15,14 +15,17 @@ public class HashTable {
 			}
 		}
 		Set<String> stringFrequency = frequency.keySet();
-		System.out.println("Frequency Of Each word in Sentence: \n 'To Be Or Not To Be'");
+		System.out.println("Frequency of words in sentence 'Paranoids are not paranoid because"
+				+ " they are paranoid but because they keep putting themselves deliberately into"
+				+ " paranoid avoidable situations' is");
 		for (String word : stringFrequency) {
-			System.out.println(word + " = " + frequency.get(word) + " times.");
-
+			if (frequency.get(word) > 1)
+				System.out.println(word + " = " + frequency.get(word) + " times.");
 		}
 	}
 
 	public static void main(String[] args) {
-		userInputSentence("To Be Or Not To Be");
+		userInputSentence("Paranoids are not paranoid because they are paranoid but because "
+				+ "they keep putting themselves deliberately into paranoid avoidable situations");
 	}
 }
